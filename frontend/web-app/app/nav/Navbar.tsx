09680386c8b694027d9a09ded67b5e6b74ba79cc
Emircan Duman <emircandum10@gmail.com>
2025-10-08 13:55:14 +0300

    SignalR Config and web app dockerization

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,17 +1,19 @@
+'use client';
+
 import Search from './Search';
 import Logo from './Logo';
 import LoginButton from './LoginButton';
-import { getCurrentUser } from '../actions/authActions';
 import UserActions from './UserActions';
+import { useSession } from 'next-auth/react';
 
-export default async function Navbar() {
-    const user = await getCurrentUser();
+export default function Navbar() {
+    const session = useSession();
     return (
         <header className='sticky top-0 z-50 flex justify-between bg-white shadow-md py-5 px-5 items-center text-gray-800'>
             <Logo />
             <Search />
-            {user ? (
-                <UserActions user={user} />
+            {session.data?.user ? (
+                <UserActions user={session.data.user} />
             ) : (
                 <LoginButton />
             )}
--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -4,20 +4,22 @@ import { useAuctionStore } from '@/hooks/useAuctionStore'
 import { useBidStore } from '@/hooks/useBidStore'
 import { Auction, AuctionFinished, Bid } from '@/types'
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr'
-import { User } from 'next-auth'
 import { useParams } from 'next/navigation'
 import React, { ReactNode, useCallback, useEffect, useRef } from 'react'
 import toast from 'react-hot-toast'
 import AuctionCreatedToast from '../conponents/AuctionCreatedToast'
 import { getDetailedViewData } from '../actions/auctionActions'
 import AuctionFinishedToast from '../conponents/AuctionFinishedToast'
+import { useSession } from 'next-auth/react'
 
 type Props = {
     children: ReactNode
-    user: User | null
 }
 
-export default function SignalRProvider({ children, user }: Props) {
+export default function SignalRProvider({ children }: Props) {
+    const session = useSession();
+    const user = session.data?.user;
+
     const connection = useRef<HubConnection | null>(null);
     const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
@@ -57,9 +59,9 @@ export default function SignalRProvider({ children, user }: Props) {
     useEffect(() => {
         if (!connection.current) {
             connection.current = new HubConnectionBuilder()
-                .withUrl('http://localhost:6001/notifications')
-                .withAutomaticReconnect()
-                .build();
+              .withUrl(process.env.NEXT_PUBLIC_NOTIFY_URL!)
+              .withAutomaticReconnect()
+              .build();
 
             connection.current.start()
                 .then(() => 'Connected to notification hub')
